ba0e41f1b80f49a1e298e4a562a2ff025ead3d09
Nhut Nguyen <nhut.nguyen@edge-works.net>
2018-03-26 23:44:41 +0700

    Build IntelliJ IDE

---
+++ librarys/library/WebContent/resources/js/login.js
@@ -0,0 +1,20 @@
+$(function() {
+	
+	/*Variable*/
+	let test = "test";
+	
+	/*Function main*/
+	let setup = function() {
+		alert("Hello!" + test);
+		func();
+	}
+	
+	/*Function here*/
+	let func = function() {
+		$("#test").click(function(){
+	        $(this).hide();
+	    });
+	}
+	return setup();
+	
+});
---
+++ librarys/library/src/vn/dntu/library/controller/DemoController.java
@@ -0,0 +1,19 @@
+package vn.dntu.library.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DemoController {
+	
+	@RequestMapping(value = "/welcome.do", method = RequestMethod.GET)
+	public String welcome() {
+        return "welcome";
+	}
+	
+	@RequestMapping(value = "/test.do", method = RequestMethod.GET)
+	public String test() {
+        return "/footer";
+	}
+}
